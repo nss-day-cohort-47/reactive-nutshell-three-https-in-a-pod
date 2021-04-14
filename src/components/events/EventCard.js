e58bd106358f3a2e90832eaa21ed0ee9c2536f6e
Logan Demmy <frank.demmy@gmail.com>
2021-04-14 11:44:54 -0500

    user filtered with italics and background

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -6,6 +6,19 @@ import { Link } from 'react-router-dom'
 
 export const EventCard = ({event, deleteSetEvent, loggedInUser, index}) => {
     return (
+        <>
+        {loggedInUser === event.userId ? <><article className="user-event-card">
+            {index === 0 ? <h2 className="user-nextevent">Next Event</h2>: ""}
+            <h3 className="user-event-title">{event.title}</h3>
+            <p className="user-event-message">{event.message}</p>
+            <p className="user-event-eventdate">Date: {event.eventdate} at {event.eventtime} </p>
+            <p className="user-event-location">Location: {event.location}</p>
+            <p className="user-event-posted">Posted by: {event.user.name} </p>
+                <Link to={`/events/${event.id}/edit`}><button >Edit</button></Link>
+                <button type="button" onClick={() => deleteSetEvent(event.id)}>Delete</button> 
+            {index === 0 ? <><hr /><hr /></>: ""}
+        </article>
+        </> : <>
         <article className="event-card">
             {index === 0 ? <h2 className="nextevent">Next Event</h2>: ""}
             <h3 className="event-title">{event.title}</h3>
@@ -13,12 +26,10 @@ export const EventCard = ({event, deleteSetEvent, loggedInUser, index}) => {
             <p className="event-eventdate">Date: {event.eventdate} at {event.eventtime} </p>
             <p className="event-location">Location: {event.location}</p>
             <p className="event-posted">Posted by: {event.user.name} </p>
-            {loggedInUser === event.userId ? 
-            <>
-                <Link to={`/events/${event.id}/edit`}><button >Edit</button></Link>
-                <button type="button" onClick={() => deleteSetEvent(event.id)}>Delete</button> 
-            </>: ""}
-            {index === 0 ? <><hr /><hr /></>: ""}
+        {index === 0 ? <><hr /><hr /></>: ""}
         </article>
+        </>
+        }   
+    </>
     )
 }
