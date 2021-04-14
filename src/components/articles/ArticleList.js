3c82ccd03769f7ec019331b76e5ee805daa1a20c
HPreast <preasths1@gmail.com>
2021-04-14 15:33:07 -0500

    Now corectly displays articles of logged in user and friends of that user, user has the ability to delete articles

--- src/components/articles/ArticleList.js
+++ src/components/articles/ArticleList.js
@@ -8,58 +8,52 @@ import { ArticleCard } from "./ArticleCards"
 
 export const ArticleList = () => {
     const [articles, setArticles] = useState([]);
-    const [friendArticles, setFriendArticles] =useState([]);
+   
 
     const history = useHistory();
 
     const loggedInUser = JSON.parse(sessionStorage.getItem("nutshell_user"))
-    
-    function numberToArray(number) {
-        let array = number.toString().split("");
-        return array.map(x => parseInt(x))
-    }
-    let currentUser = numberToArray(loggedInUser)
-    // const getCurrentArticle = () => {
-    //     return getUserArticles(loggedInUser)
-    //     .then(articlesFromAPI => {
-    //         setArticles(articlesFromAPI)
-    //     })
-    // }
-    
-    const getFriendArticles = () => {
-        getUserFriends(loggedInUser)
-        .then(result => {
-            const friends = result
-            const friendId = friends.map((elem) => elem.user.id)
-            const articlePublisherArray = currentUser.concat(friendId)
-            articlePublisherArray.forEach(user => { 
-            getUserArticles(user)
-                .then()
+
+    const getCurrentArticle = () => {
+        let userFriendArticles = [];
+        return getUserFriends(loggedInUser)
+            .then(result => {
+                
+                result.forEach(friend => {
+
+                    getUserArticles(friend.user.id)
+                        .then(articleArray => {
+                            userFriendArticles = userFriendArticles.concat(articleArray)
+                            // console.log(userFriendArticles)
+                        })
+                        .then(() => getUserArticles(loggedInUser)
+                            .then(article => {
+
+                                let allArticles = []
+                                allArticles = userFriendArticles.concat(article)
+                                setArticles(allArticles)
+                            })
+                        )
+                })
+                
             })
-            console.log(eachPublisher)
-            getUserArticles(eachPublisher)
-            .then(friendArticlesFromAPI => {
-                setFriendArticles(friendArticlesFromAPI)
-            })
-        })
-        
+
     }
 
+    
+
     const handleDeleteArticle = (articleId) => {
-        deleteArticle(articleId).then(() => 
-        // getCurrentArticle()
-        getFriendArticles()
+        deleteArticle(articleId).then(() =>
+            getCurrentArticle()
+           
         )
     }
 
-    // useEffect(() => {
-    //     getCurrentArticle()
-    // }, [])
-
     useEffect(() => {
-        getFriendArticles()
+        getCurrentArticle()
     }, [])
 
+    
     function biggestToSmallest(a, b) {
         return b.timestamp - a.timestamp;
     }
@@ -68,24 +62,15 @@ export const ArticleList = () => {
     return (
         <section className="articleList">
             <button type="button" className="btn"
-            onClick={() => {history.push("/articles/create")}}>
+                onClick={() => { history.push("/articles/create") }}>
                 Add Article
             </button>
             <div className="articleCards">
-                {articles.map(article => 
-                    <ArticleCard 
-                    key={article.id}
-                    article={article}
-                    handleDeleteArticle={handleDeleteArticle}
-                    />
-                )}
-            </div>
-            <div className="articleCards">
-                {friendArticles.map(article => 
-                    <ArticleCard 
-                    key={article.id}
-                    article={article}
-                    handleDeleteArticle={handleDeleteArticle}
+                {articles.map(article =>
+                    <ArticleCard
+                        key={article.id}
+                        article={article}
+                        handleDeleteArticle={handleDeleteArticle}
                     />
                 )}
             </div>
