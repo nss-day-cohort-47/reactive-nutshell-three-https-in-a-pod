0738642ecfdcc913f6e1fb3624fbec5b4c1f17f6
HPreast <preasths1@gmail.com>
2021-04-14 13:38:22 -0500

    Added logic to display friends articles

--- src/components/articles/ArticleList.js
+++ src/components/articles/ArticleList.js
@@ -2,11 +2,13 @@
 import React, { useEffect, useState } from "react"
 import { useHistory } from "react-router";
 import { deleteArticle, getUserArticles } from "../../modules/articleManager";
+import { getUserFriends } from "../../modules/friendsListManager";
 import { ArticleCard } from "./ArticleCards"
 
 
 export const ArticleList = () => {
     const [articles, setArticles] = useState([]);
+    const [friendArticles, setFriendArticles] =useState([]);
 
     const history = useHistory();
 
@@ -20,6 +22,21 @@ export const ArticleList = () => {
             })
     }
 
+    const getFriendArticles = () => {
+        getUserFriends(loggedInUser)
+        .then(result => {
+            const friends = result
+            const friendId = friends.map((elem) => elem.user.id)
+            // console.log(friendId)
+            getUserArticles(friendId)
+            .then(friendArticlesFromAPI => {
+                console.log(friendArticlesFromAPI)
+                setFriendArticles(friendArticlesFromAPI)
+            })
+        })
+        
+    }
+
     const handleDeleteArticle = (articleId) => {
         deleteArticle(articleId).then(() => 
         getCurrentArticle()
@@ -28,6 +45,10 @@ export const ArticleList = () => {
 
     useEffect(() => {
         getCurrentArticle()
+    }, [])
+
+    useEffect(() => {
+        getFriendArticles()
     }, [])
 
     function biggestToSmallest(a, b) {
@@ -43,6 +64,15 @@ export const ArticleList = () => {
             </button>
             <div className="articleCards">
                 {articles.map(article => 
+                    <ArticleCard 
+                    key={article.id}
+                    article={article}
+                    handleDeleteArticle={handleDeleteArticle}
+                    />
+                )}
+            </div>
+            <div className="articleCards">
+                {friendArticles.map(article => 
                     <ArticleCard 
                     key={article.id}
                     article={article}
