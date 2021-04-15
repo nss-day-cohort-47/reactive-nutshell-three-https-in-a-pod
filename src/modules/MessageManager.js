13a6b3dca9647c45f1b05c62a71c7cbd8fc4b379
EthanMathis <ethan.mathis@outlook.com>
2021-04-15 10:45:23 -0500

    checking PRs

--- src/modules/MessageManager.js
+++ src/modules/MessageManager.js
@@ -2,13 +2,24 @@
 const url = "http://localhost:8088"
 
 export const getAllMessages = () => {
-    return fetch(`${url}/messages`)
+    return fetch(`${url}/messages?_expand=user`)
     .then(response => response.json())
 }
 
 export const deleteMessage = (id) => {
     return fetch(`${url}/messages/${id}`, {
         method: "DELETE"
+    })
+    .then(response => response.json())
+}
+
+export const addMessage = (newMessage) => {
+    return fetch(`${url}/messages`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newMessage)
     })
     .then(response => response.json())
 }
