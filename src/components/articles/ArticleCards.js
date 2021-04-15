f2b7f52a9be475b072aed172c8a8d682d6938a3e
HPreast <preasths1@gmail.com>
2021-04-15 10:31:03 -0500

    Styled friends articles to look different from the current user's articles, implemented logout button

--- src/components/articles/ArticleCards.js
+++ src/components/articles/ArticleCards.js
@@ -1,6 +1,7 @@
 //Hunter Preast-- Create cards to display posted articles
 import React from "react"
 import { Link } from 'react-router-dom'
+import "./Articles.css"
 
 export const ArticleCard = ({article, handleDeleteArticle, loggedInUser}) => {
     
@@ -9,6 +10,7 @@ export const ArticleCard = ({article, handleDeleteArticle, loggedInUser}) => {
     <>    
         {loggedInUser === article.userId ?
         <>
+        <article className="article-card">
         <h4>Posted by: {article.user.name}</h4>
         <article className="postedArticle">
             <h6>{article.title}</h6>
@@ -17,7 +19,9 @@ export const ArticleCard = ({article, handleDeleteArticle, loggedInUser}) => {
             <Link to={`/articles/${article.id}/edit`}><button>Edit</button></Link>
             <button type="button" onClick={() => handleDeleteArticle(article.id)}>Delete</button>
         </article>
+        </article>
         </> : <>
+        <article className="friend-article-card">
         <h4>Posted by: {article.user.name}</h4>
         <article className="postedArticle">
             <h6>{article.title}</h6>
@@ -25,6 +29,7 @@ export const ArticleCard = ({article, handleDeleteArticle, loggedInUser}) => {
             <a href={article.url}>{article.url}</a>
             
         </article> 
+        </article>
         </>
         }
     </>    
